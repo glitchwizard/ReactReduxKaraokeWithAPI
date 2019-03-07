a19df3f17330be928deeb73d7e089be3f0b65d26
Student <example@example.com>
2019-03-06 16:32:55 -0800

    remove uncessesary code
    Co-Authored-By: Charley <glitchwizard@gmail.com>

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -13,11 +13,6 @@ function SongSearch({ dispatch }){
           return;
         }
         dispatch(fetchSongId(input.value.trim()));
-
-        // ...we'll add more custom logic here later...
-        console.log('SEARCHED ARTIST:');
-        console.log(input.value.trim());
-        //...instead of these console.log()s....
         input.value = '';
       }}>
         <input placeholder="Song Title" ref={node => {
