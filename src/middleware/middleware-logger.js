cb965b7aedda7555de9f9350a979b66ff4fe56ac
Student <example@example.com>
2019-03-06 16:35:13 -0800

    comment out console logs
    Co-Authored-By: Charley <glitchwizard@gmail.com>

--- src/middleware/middleware-logger.js
+++ src/middleware/middleware-logger.js
@@ -1,8 +1,9 @@
 const middlewareLogger = store => next => action => {
-  console.log('Originalstate: ', store.getState());
+  {/*  console.log('Originalstate: ', store.getState());
   console.log('Current Actions', action);
   next(action);
-  console.log('New Updated State: ', store.getState());
+  console.log('New Updated State: ', store.getState()); */}
+  next(action);
 };
 
 export default middlewareLogger;
